5684e9d8f1029d053751017dd1fd52cfa073a426
reticent-monolith <ben@reticent-monolith.com>
2020-12-21 10:24:15 +0000

    statblock styling; stat template change

--- modules/system/register-helpers.js
+++ modules/system/register-helpers.js
@@ -5,4 +5,9 @@ export default async function registerHandlebarsHelpers() {
         LOGGER.trace(`Calling ifEquals Helper | Arg1:${arg1} Arg2:${arg2}`);
         return (arg1.toLowerCase() == arg2.toLowerCase()) ? options.fn(this) : options.inverse(this);
     });
+
+    Handlebars.registerHelper("loud", function(string) {
+      return string.toUpperCase()
+    });
+    
 }
