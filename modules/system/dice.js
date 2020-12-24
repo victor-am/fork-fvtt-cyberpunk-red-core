66c0de2d3d28582ad8bc612a84f2a385fb88ef2b
Jason-Terry <jasonalanterry@gmail.com>
2020-12-24 01:19:38 -0500

    Implment first pass of BaseRoll and DamageRoll

--- modules/system/dice.js
+++ modules/system/dice.js
@@ -12,42 +12,75 @@ import LOGGER from "../utils/cpr-logger.js";
 
 // Base roll should calculate Critical by default.
 // Base roll should expect a stat value, a skillBase value, and a list of mods to apply.
-export async function BaseRoll(calculateCritical = true, stat = 0, skillBase = 0, mods = []) {
-  LOGGER.trace(`Calling getProp Helper | Dice BaseRoll | Arg1:${arg1} Arg2:${arg2}, Arg3:${arg3}, Arg4:${arg4}`);
-    // SKILL.val + STAT.val + [MODS]
-    // 5 + 4 + sum([-2, 1, 1])
+export async function BaseRoll(calculateCritical = true, stat = 0, skillLevel = 0, mods = []) {
+  LOGGER.trace(`Calling BaseRoll | Dice BaseRoll | Arg1:${calculateCritical} Arg2:${stat}, Arg3:${skillLevel}, Arg4:${mods}`);
 
-    // todo getChatDialog??
+  // TODO- Roll Results could be constructed as objects outside of this.
+  let rollResult = {
+    criticalRoll: 0,
+    initialRoll: new Roll(`1d10`).roll(),
+    finalRollResult: 0,
+    resultTotal: 0,
+    resultMods: mods
+  };
 
-    let rollResult = {};
-    let criticalRoll = 0;
-    let initialRoll = new Roll(`1d10`).roll(); // Use input roll if exists, otherwise, roll randomly (used for editing a test result)
-    
-    // Adjust for crit
-    if (calculateCritical) {
-      LOGGER.debug(`Checking Critical Chance | Dice BaseRoll | Initial Roll:${initialRoll}`);
-      if (roll._total == 1) {
-        criticalRoll -= new Roll(`1d10`).roll();
-        LOGGER.debug(`Critical Failure! | Dice BaseRoll | Critical Roll:${criticalRoll}`);
-      }
-      if (roll._total == 10) {
-        criticalRoll += new Roll("1d10 + ").roll();
-        LOGGER.debug(`Critical Success | Dice BaseRoll | Critical Roll:${criticalRoll}`);
-      }
+  // Adjust for crit
+  if (calculateCritical) {
+    LOGGER.debug(`Checking Critical Chance | Dice BaseRoll | Initial Roll:${rollResult.initialRoll}`);
+    if (roll._total == 1) {
+      rollResult.criticalRoll -= new Roll(`1d10`).roll();
+      LOGGER.debug(`Critical Failure! | Dice BaseRoll | Critical Roll:${rollResult.criticalRoll}`);
     }
+    if (roll._total == 10) {
+      rollResult.criticalRoll += new Roll("1d10 + ").roll();
+      LOGGER.debug(`Critical Success | Dice BaseRoll | Critical Roll:${rollResult.criticalRoll}`);
+    }
+  }
 
-    // Build results object for the dialog.
-    rollResult.initialRoll = initialRoll;
-    rollResult.criticalRoll = criticalRoll;
-    rollResult.rollMods = mods;
-    rollResult.result = initialRoll + criticalRoll + sum(mods)
-    LOGGER.debug(`Roll Result | Dice BaseRoll`);
-    console.log(rollResult);
-    return rollResult;
+  // TODO-- Move util function to a util class?
+  let numOr0 = n => isNaN(n) ? 0 : n
+  // Build results object for the dialog.
+  rollResult.finalRollResult += initialRoll._total + criticalRoll_total;
+  rollResult.resultTotal += roll.finalRollResult + skillLevel + stat + mods.reduce((a, b) => numOr0(a) + numOr0(b));
+  
+  LOGGER.debug(`Roll Result | Dice BaseRoll`);
+  console.log(rollResult);
+  return rollResult;
 }
 
-export function DamageRoll() {
+export function DamageRoll(rollFormula = '1d6', location = "body") {
+  LOGGER.trace(`Calling DamageRoll | Dice DamageRoll | Arg1:${rollFormula}`);
 
+  // TODO- Roll Results could be constructed as objects outside of this.
+  let rollResult = {
+    isCriticalInjury: false,
+    location: location,
+    criticalInjury: "",
+    initialRoll: new Roll(rollFormula).roll(),
+    finalResult = 0
+  };
+
+  // Check for sixes choom!
+  let sixes = 0;
+  for (let roll in rollResult.initialRoll.terms.results) {
+    LOGGER.debug(`Checking for Two Sixes | Dice DamageRoll | Current Result:${result}`);
+    if (roll.result == 6) {
+      sixes++
+    }
+  }
+
+  // If sixes, set some info on this sick critical injury choom!
+  if (sixes >= 2) {
+    LOGGER.debug(`Critical Injury! | Dice DamageRoll | Sixes:${sixes}`);
+    rollResult.isCriticalInjury = true;
+    rollResult.criticalInjury = "Something Bad";
+  }
+
+  // Build results object for the dialog.
+  rollResult.finalResult += initialRoll._total;
+  LOGGER.debug(`Roll Result | Dice BaseRoll`);
+  console.log(rollResult);
+  return rollResult;
 }
 
 export function DeathSave() {
@@ -57,8 +90,3 @@ export function DeathSave() {
 export function Initiative() {
 
 }
-
-// BASE: 1d10(w crits) + SOME + SOME + MOD
-// DMG: xd6(two or more six, crit injury table) 
-// DEATH: 1d10(die on 10) 
-// I
