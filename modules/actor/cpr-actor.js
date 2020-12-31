6d8d823407bf4f97e375413024c9acb85c40ba81
Jason-Terry <jasonalanterry@gmail.com>
2020-12-30 20:08:41 -0500

    Adjust template, fix logging in actor sheet, move derived stats to actor.

--- modules/actor/cpr-actor.js
+++ modules/actor/cpr-actor.js
@@ -11,11 +11,18 @@ export default class CPRActor extends Actor {
   /** @override */
   prepareData() {
     LOGGER.trace("Prepare Data | CPRActor | Called.");
+    LOGGER.debug(this);
     super.prepareData();
     const actorData = this.data;
     actorData.filteredItems = this.itemTypes;
+
     LOGGER.debug("Prepare Character Data | CPRActor | Checking on contents of `filteredItems`.");
     console.log(actorData.filteredItems);
+    
+    // Prepare data for both types
+    this._calculateDerivedStats(actorData);
+
+    // Prepare type data
     if (actorData.type === 'character') this._prepareCharacterData(actorData);
     if (actorData.type === 'mook') this._prepareMookData(actorData); 
   }
@@ -51,5 +58,26 @@ export default class CPRActor extends Actor {
     console.log(data)
   }
 
-  
+  _calculateDerivedStats(actorData) {
+    // Calculate MAX HP
+    LOGGER.trace(`ActorID _calculateDerivedStats | CPRActorSheet | Called.`);
+    let stats = actorData.stats;
+    let derivedStats = actorData.derivedStats;
+
+    // Set max HP
+    derivedStats.hp.max = 10 + 5*(Math.ceil((stats.will.value + stats.body.value) / 2));
+    if (derivedStats.hp.value > derivedStats.hp.max) derivedStats.hp.value = derivedStats.hp.max;
+
+    // Seriously wounded
+    derivedStats.seriouslyWounded = Math.ceil(hp.max / 2);
+
+    // Death save
+    derivedStats.deathSave = body.value;
+    
+    // Max Humanity
+    // TODO-- Subtract installed cyberware...
+    hum.max = 10 * emp.value;
+    if (hum.value > hum.max) hum.value = hum.max;
+  }
+
 }
--- modules/actor/sheet/cpr-actor-sheet.js
+++ modules/actor/sheet/cpr-actor-sheet.js
@@ -12,7 +12,7 @@ export default class CPRActorSheet extends ActorSheet {
 
   /** @override */
   static get defaultOptions() {
-    LOGGER.trace("Default Options | CPRActorSheet | Called.");
+    LOGGER.trace("ActorID defaultOptions | CPRActorSheet | Called.");
     return mergeObject(super.defaultOptions, {
       classes: super.defaultOptions.classes.concat(["sheet", "actor"]),
       width: 600,
@@ -24,7 +24,7 @@ export default class CPRActorSheet extends ActorSheet {
   /** @override */
   getData() {
     // TODO - Understand how to use getData and when.
-    LOGGER.trace("Get Data | CPRActorSheet | Called.");
+    LOGGER.trace("AcotrID getData | CPRActorSheet | Called.");
     const data = super.getData();
     this._addConfigData(data);
     return data;
@@ -54,17 +54,14 @@ export default class CPRActorSheet extends ActorSheet {
     html.find('.add-skill').click(event => this._addSkill(event));
   }
 
-  
-
-
-  /*
-    INTERNAL METHODS BELOW HERE
-  */
-
   /* -------------------------------------------- */
+  //  INTERNAL METHODS BELOW HERE
+  /* -------------------------------------------- */
+
+
   _addConfigData(sheetData) {
     // TODO - sheetData config additions should be added in a less procedural way.
-    LOGGER.trace(`Add Config Data | CPRActorSheet | Called with ${this}.`);
+    LOGGER.trace(`ActorID _addConfigData | CPRActorSheet | Called.`);
     sheetData.skillCategories = CPR.skillCategories;
     sheetData.statList = CPR.statList;
     sheetData.skillDifficulties = CPR.skillDifficulties;
@@ -74,27 +71,10 @@ export default class CPRActorSheet extends ActorSheet {
     sheetData.weaponTypeList = CPR.weaponTypeList;
     sheetData.ammoVariety = CPR.ammoVariety;
   }
-  
-  _calculateDerivedStats(data) {
-    // Calculate MAX HP
-    let will = data.data.stats.will;
-    let hp = data.data.derivedStats.hp;
-    let body = data.data.stats.body;
-    let hum = data.data.humanity;
-    let emp = data.data.stats.emp;
-    hp.max = 10 + 5*(Math.ceil((will.value + body.value) / 2));
-    if (hp.value > hp.max) hp.value = hp.max;
-    // Humanity
-    hum.max = 10 * emp.value;
-    if (hum.value > hum.max) hum.value = hum.max;
-    // Seriously wounded
-    data.data.derivedStats.seriouslyWounded = Math.ceil(hp.max / 2);
-    // Death save
-    data.data.derivedStats.deathSave = body.value;
-  }
 
+  // TODO - Function is getting far to long, we need to find ways to condense it.
   async _onRoll(event) {
-    LOGGER.trace(`Actor _onRoll | .rollable click | Called.`);
+    LOGGER.trace(`ActorID _onRoll | CPRActorSheet | Called.`);
 
     // TODO - Cleaner way to init all this fields?
     // TODO - Create a input object to encompass these fields?
@@ -105,7 +85,9 @@ export default class CPRActorSheet extends ActorSheet {
     
     let rollType = $(event.currentTarget).attr("data-roll-type");
     let rollTitle = $(event.currentTarget).attr("data-roll-title");
-    const itemId = this._getItemId(event);
+
+    // If we are rolling a stat this will be null... SHould only 
+    
 
     // Do I need this?
     let actorData = this.getData();
@@ -121,23 +103,27 @@ export default class CPRActorSheet extends ActorSheet {
     switch (rollType) {
       case "stat": {
         statValue = actorData.data.stats[rollTitle].value;
-        LOGGER.trace(`Actor _onRoll | rolling stat: ` + rollTitle + ` | ` + statValue);
+        LOGGER.trace(`ActorID _onRoll | rolling ${rollTitle} | Stat Value: ${statValue}`);
         break;
       }
       case "skill": {
-        const item = this.actor.items.find(i => i.data._id == itemId);
+        const itemId = this._getItemId(event);
+        const item = 
         statValue = actorData.data.stats[item.data.data.stat].value;
         skillValue = item.data.data.level;
-        LOGGER.trace(`Actor _onRoll | rolling skill: ` + rollTitle + ` | ` + skillValue);
+        LOGGER.trace(`ActorID _onRoll | rolling ${rollTitle} | Stat Value: ${statValue} + Skill Value:${skillValue}`);
         break;
       }
       case "roleAbility": {
         const roleInfo = actorData.data.roleInfo;
         const role = roleInfo["role"];
         skillValue = roleInfo.roleskills[role][rollTitle];
-        LOGGER.trace(`Actor _onRoll | rolling ability: ` + rollTitle + ` | ` + skillValue);
+        LOGGER.trace(`ActorID _onRoll | rolling ability: ` + rollTitle + ` | ` + skillValue);
         break;
       }
+      // Q: Do we ever need to cancel a roll? 
+      // This really only applys if we display the mods dialog, and then they wish to NOT enter a mod.
+      // If we want to have this really be a function of the system, we should ALWAYS display the dialog, as it's the only control available to trigger canceling a roll.
       case "cancel": {
         // Catch all if we want a way to cancel out of a roll.
         return;
@@ -147,28 +133,35 @@ export default class CPRActorSheet extends ActorSheet {
     RollCard(BaseRoll(statValue, skillValue, totalMods, rollCritical));
   }
 
+  // TODO - We should go through the following, and assure all private methods can be used outside of the context of UI controls as well.
+
   _updateItem(event) {
-    LOGGER.trace(`Actor _itemUpdate | .item-edit click | Called.`);
+    LOGGER.trace(`ActorID _itemUpdate | CPRActorSheet | Called.`);
     let itemId = this._getItemId(event);
-    LOGGER.debug(`Actor _itemUpdate | Item ID:${itemId}.`);    
+    LOGGER.debug(`ActorID _itemUpdate | Item ID:${itemId}.`);    
     const item = this.actor.items.find(i => i.data._id == itemId)
     console.log(item);
     item.sheet.render(true);
   }
 
   _getItemId(event) {
+    LOGGER.trace(`ActorID _getItemId | CPRActorSheet | Called.`);
     return $(event.currentTarget).attr("data-item-id")
   }
 
+  _getOwnedItem(itemId) {
+    return this.actor.items.find(i => i.data._id == itemId);
+  }
+
   _deleteOwnedItem(event) {
-    LOGGER.trace(`Actor _deleteOwnedItem | .item-delete click | Called.`);
+    LOGGER.trace(`ActorID _deleteOwnedItem | CPRActorSheet | Called.`);
     let itemId = this._getItemId(event);
     let itemList = this.actor.items;
     itemList.forEach(item => { if (item.data._id === itemId) item.delete() });
   }
 
   _addSkill() {
-    LOGGER.trace(`Actor _addSkill | .add-skill click | called.`);
+    LOGGER.trace(`ActorID _addSkill | CPRActorSheet | called.`);
     let itemData = {
       name: "skill", type: 'skill', data: {}
     };
