ce198f38991f43f7e31084a679b5edaa200dffda
Jason-Terry <jasonalanterry@gmail.com>
2021-01-25 18:28:49 -0500

    Fixing merge.

--- modules/utils/cpr-misc.js
+++ modules/utils/cpr-misc.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-export class CPRArrayUtils {
-    static PushMultipleNumbersFromString(array, string, delims) {
-        array.push(...string.split(new RegExp(delims.join('|'), '')));
-        let result = array.map(function (x) {
-            return parseInt(x);
-        });
-
-        return result;
-    }
-}
-=======
 export default class CPRArrayUtils {
   static PushMultipleNumbersFromString(array, string, delims) {
     array.push(...string.split(new RegExp(delims.join("|"), "")));
@@ -17,4 +5,3 @@ export default class CPRArrayUtils {
     return result;
   }
 }
->>>>>>> dev
