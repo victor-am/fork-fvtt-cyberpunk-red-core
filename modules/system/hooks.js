6373660fda6c779c193ad96cc9367a0c5d3b8b04
Darin Sorrentino <dsorrent@obsidian.homelab.net>
2021-01-27 15:15:04 -0500

    Created stubs for combat hooks for later work.

--- modules/system/hooks.js
+++ modules/system/hooks.js
@@ -1,5 +1,7 @@
 import * as actorHooks from "../hooks/actor.js";
+import * as combatHooks from "../hooks/combat.js";
 
 export default function registerHooks() {
   actorHooks.default();
+  combatHooks.default();
 }
