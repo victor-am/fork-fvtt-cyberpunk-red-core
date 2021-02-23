9c10cae5fbdd0c66921d05f40df90950e7a994a0
dsorrent <Darin.Sorrentino@gmail.com>
2021-02-23 11:33:34 -0500

    Moved the data validation for the RulesLawyer validRole into the validRole function since that made more sense in case it was called from elsewhere.

--- modules/utils/cpr-rules.js
+++ modules/utils/cpr-rules.js
@@ -9,39 +9,43 @@ export default class Rules {
   }
 
   static validRole(actor, data) {
-    const newRoleData = data.data.roleInfo;
-    const changedRoles = newRoleData.roles;
     let roleValid = true;
-    if ("solo" in changedRoles || "tech" in changedRoles || "medtech" in changedRoles) {
-      const currentRoleData = actor.data.data.roleInfo;
-      mergeObject(currentRoleData, newRoleData);
-      let abilityRank = 0;
-      let subskillRank = 0;
-      Object.keys(changedRoles).forEach((role) => {
-        const roleSkills = currentRoleData.roleskills[role];
-        Object.keys(roleSkills).forEach((roleSkill) => {
-          if (roleSkill === "subSkills") {
-            Object.keys(roleSkills.subSkills).forEach((subSkill) => {
-              subskillRank += roleSkills.subSkills[subSkill];
+    if (typeof updatedData.data !== "undefined") {
+      if (typeof updatedData.data.roleInfo !== "undefined") {
+        const newRoleData = data.data.roleInfo;
+        const changedRoles = newRoleData.roles;
+        if ("solo" in changedRoles || "tech" in changedRoles || "medtech" in changedRoles) {
+          const currentRoleData = actor.data.data.roleInfo;
+          mergeObject(currentRoleData, newRoleData);
+          let abilityRank = 0;
+          let subskillRank = 0;
+          Object.keys(changedRoles).forEach((role) => {
+            const roleSkills = currentRoleData.roleskills[role];
+            Object.keys(roleSkills).forEach((roleSkill) => {
+              if (roleSkill === "subSkills") {
+                Object.keys(roleSkills.subSkills).forEach((subSkill) => {
+                  subskillRank += roleSkills.subSkills[subSkill];
+                });
+              } else {
+                abilityRank = roleSkills[roleSkill];
+              }
             });
-          } else {
-            abilityRank = roleSkills[roleSkill];
-          }
-        });
-        // Normalize, after this, abilityRank should equal subskillRank otherwise invalid
-        switch (role) {
-          case "tech": {
-            abilityRank *= 2;
-            break;
-          }
-          case "medtech":
-          case "solo":
-          default:
+            // Normalize, after this, abilityRank should equal subskillRank otherwise invalid
+            switch (role) {
+              case "tech": {
+                abilityRank *= 2;
+                break;
+              }
+              case "medtech":
+              case "solo":
+              default:
+            }
+            if (abilityRank < subskillRank) {
+              roleValid = false;
+            }
+          });
         }
-        if (abilityRank < subskillRank) {
-          roleValid = false;
-        }
-      });
+      }
     }
     return roleValid;
   }
