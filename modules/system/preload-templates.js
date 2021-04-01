b3ffeb7224450fcf8660b50b64e5d235789e3dec
dsorrent <Darin.Sorrentino@gmail.com>
2021-04-01 00:06:05 -0400

    cpr-rolls was referencing some base roll dialog which doesn't exist so I changed this to the generic one.  Preload templates was trying to pre-load an attack template which doesn't exist either.

--- modules/system/preload-templates.js
+++ modules/system/preload-templates.js
@@ -13,7 +13,6 @@ export default function preloadHandlebarsTemplates() {
     "systems/cyberpunk-red-core/templates/dialog/cpr-install-cyberware-prompt.hbs",
     "systems/cyberpunk-red-core/templates/dialog/cpr-load-ammo-prompt.hbs",
     "systems/cyberpunk-red-core/templates/dialog/cpr-select-compatible-ammo-prompt.hbs",
-    "systems/cyberpunk-red-core/templates/dialog/rolls/cpr-verify-roll-attack-prompt.hbs",
     "systems/cyberpunk-red-core/templates/dialog/rolls/cpr-verify-roll-damage-prompt.hbs",
     "systems/cyberpunk-red-core/templates/dialog/rolls/cpr-verify-roll-deathsave-prompt.hbs",
     "systems/cyberpunk-red-core/templates/dialog/rolls/cpr-verify-roll-generic-prompt.hbs",
