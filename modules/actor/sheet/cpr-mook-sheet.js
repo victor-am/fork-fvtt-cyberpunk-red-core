76de5e3faa5774987c0d5362957d2d4c3f3a26f6
Jay Greguske <jgreguske@redhat.com>
2021-04-08 22:23:51 -0400

    redesign weapon pane and add more listeners

--- modules/actor/sheet/cpr-mook-sheet.js
+++ modules/actor/sheet/cpr-mook-sheet.js
@@ -1,4 +1,4 @@
-/* global mergeObject */
+/* global mergeObject getProperty $ */
 import CPRActorSheet from "./cpr-actor-sheet.js";
 import ModMookSkillPrompt from "../../dialog/cpr-mod-mook-skill-prompt.js";
 import LOGGER from "../../utils/cpr-logger.js";
@@ -17,21 +17,23 @@ export default class CPRMookActorSheet extends CPRActorSheet {
     });
   }
 
-  /** @override */
-  getData() {
-    LOGGER.trace("getData | CPRMookActorSheet | Called.");
-    const data = super.getData();
-    return data;
-  }
-
   activateListeners(html) {
     LOGGER.trace("activateListeners | CPRMookActorSheet | Called.");
+    // clicking
     html.find(".ablate").click((event) => this._ablateArmor(event));
-    html.find(".fire-checkbox").click((event) => this._fireCheckboxToggle(event));
+    html.find(".mook-fire-mode").click((event) => this._fireCheckboxToggle(event));
+    html.find(".item-action").click((event) => this._itemAction(event));
     html.find(".item-edit").click((event) => this._renderItemCard(event));
     html.find(".mod-mook-skill").click(() => this._modMookSkill());
     html.find(".reset-value").click((event) => this._resetActorValue(event));
     html.find(".rollable").click((event) => this._onRoll(event));
+
+    // dragging
+    const handler = (event) => this._onDragItemStart(event);
+    html.find(".item").each((i, li) => {
+      li.setAttribute("draggable", true);
+      li.addEventListener("dragstart", handler, false);
+    });
   }
 
   async _modMookSkill() {
@@ -49,5 +51,33 @@ export default class CPRMookActorSheet extends CPRActorSheet {
       SystemUtils.DisplayMessage("notify", msg);
       again = formData.again;
     }
+  }
+
+  /** @override */
+  _fireCheckboxToggle(event) {
+    LOGGER.trace("_fireCheckboxToggle Called | CPRMookActorSheet | Called.");
+    const weaponID = $(event.currentTarget).attr("data-item-id");
+    const firemode = $(event.currentTarget).attr("data-fire-mode");
+    const flag = getProperty(this.actor.data, `flags.cyberpunk-red-core.firetype-${weaponID}`);
+    LOGGER.debugObject(this.actor);
+    LOGGER.debug(`firemode is ${firemode}`);
+    LOGGER.debug(`weaponID is ${weaponID}`);
+    LOGGER.debug(`flag is ${flag}`);
+    if (flag === firemode) {
+      // if the flag was already set to firemode, that means we unchecked a box
+      this.actor.unsetFlag("cyberpunk-red-core", `firetype-${weaponID}`);
+    } else {
+      this.actor.setFlag("cyberpunk-red-core", `firetype-${weaponID}`, firemode);
+    }
+  }
+
+  /** @override */
+  _onDragItemStart(event) {
+    LOGGER.trace("_onDragItemStart | CPRMookActorSheet | called.");
+    super._onDragItemStart(event);
+    // auto-equip the item
+    const itemId = event.currentTarget.getAttribute("data-item-id");
+    const item = this.actor.getEmbeddedEntity("OwnedItem", itemId);
+    this._updateOwnedItemProp(item, "data.equipped", "equipped");
   }
 }
