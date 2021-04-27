1581e0995529131740659367b1952bd0913e11b2
twagner <tim1000.wagner@gmail.com>
2021-04-27 09:52:17 +0200

    Moved automatic resizing out of _render, fixed character sheet resizing on updating fields

--- modules/item/sheet/cpr-item-sheet.js
+++ modules/item/sheet/cpr-item-sheet.js
@@ -24,14 +24,12 @@ export default class CPRItemSheet extends ItemSheet {
   async _render(force = false, options = {}) {
     LOGGER.trace("ItemSheet | _render | Called.");
     await super._render(force, options);
-    const setting = game.settings.get("cyberpunk-red-core", "automaticallyResizeSheets");
-    if (setting) {
-      if (Object.entries(options).length === 0) {
-        // In case of updating an item on an actor all the items, which were ever opened from that actor sheet are called with options = { action:"update" }
-        // to prevent rendering them it is checked that the options object is empty. For changes directly on the item _delayedRender is called after the change to resize the sheet
-        this.setPosition({ width: this.position.width, height: 35 }); // Make sheet small, so this.form.offsetHeight does not include whitespace
-        this.setPosition({ width: this.position.width, height: this.form.offsetHeight + 46 }); // 30px for the header and 8px top margin 8px bottom margin
-      }
+    if (!Object.keys(options).some( (k) => { return (k === "action") && (options[k] === "update"); })) {
+      // In case of updating a value on an item sheet the resizing should not happen.
+      // If a value is updated the _render function is called with options = { action: "update" }
+      // Should one still desire resizing the sheet afterwards, please call _automaticResize explicitly.
+      // Additionally if one item owned by an actor is updated, all items, which were opened before are called with options = { action: "update" }.
+      this._automaticResize()
     }
   }
 
@@ -86,19 +84,12 @@ export default class CPRItemSheet extends ItemSheet {
     html.find(".select-compatible-ammo").click((event) => this._selectCompatibleAmmo(event));
 
     // Sheet resizing
-    html.find(".tab-label").click((event) => this._delayedRender(this));
+    html.find(".tab-label").click((event) => this._automaticResize());
   }
 
   /*
   INTERNAL METHODS BELOW HERE
 */
-  _delayedRender(sheet) {
-    // It seems that the size of the content does not change immediately upon updating the content
-    setTimeout(() => {
-      sheet._render();
-    }, 10);
-  }
-
   _itemCheckboxToggle(event) {
     LOGGER.trace("CPRItemID _itemCheckboxToggle Called | CPRItemSheet | Called.");
     const itemData = duplicate(this.item.data);
@@ -106,7 +97,7 @@ export default class CPRItemSheet extends ItemSheet {
     if (hasProperty(itemData, target)) {
       setProperty(itemData, target, !getProperty(itemData, target));
       this.item.update(itemData);
-      this._delayedRender(this);
+      this._automaticResize(); // Resize the sheet as length of settings list might have changed
     }
   }
 
@@ -125,7 +116,7 @@ export default class CPRItemSheet extends ItemSheet {
       }
       setProperty(itemData, target, prop);
       this.item.update(itemData);
-      this._delayedRender(this);
+      this._automaticResize(); // Resize the sheet as length of settings list might have changed
     }
   }
 
@@ -135,7 +126,19 @@ export default class CPRItemSheet extends ItemSheet {
     formData = await SelectCompatibleAmmo.RenderPrompt(formData);
     if (formData.selectedAmmo) {
       await this.item.setCompatibleAmmo(formData.selectedAmmo);
-      this._delayedRender(this);
+      this._automaticResize(); // Resize the sheet as length of ammo list might have changed
+    }
+  }
+
+  _automaticResize() {
+    LOGGER.trace("ItemSheet | _automaticResize | Called.");
+    const setting = game.settings.get("cyberpunk-red-core", "automaticallyResizeSheets");
+    if (setting) {
+      // It seems that the size of the content does not change immediately upon updating the content
+      setTimeout(() => {
+        this.setPosition({ width: this.position.width, height: 35 }); // Make sheet small, so this.form.offsetHeight does not include whitespace
+        this.setPosition({ width: this.position.width, height: this.form.offsetHeight + 46 }); // 30px for the header and 8px top margin 8px bottom margin
+      }, 10);
     }
   }
 }
