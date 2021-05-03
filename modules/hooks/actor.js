b02707452abc355e35131bb8f0266d424104cfdc
jalensailin <jalen.michalslevy@gmail.com>
2021-05-03 12:58:33 -0400

    ablating armor from tokenHUD updates all equipped armor in that location as well (currently only for linked tokens)

--- modules/hooks/actor.js
+++ modules/hooks/actor.js
@@ -19,7 +19,6 @@ const actorHooks = () => {
       Object.entries(updatedData.data.externalData).forEach(
         ([itemType, itemData]) => {
           if (!updatedData.data.externalData[itemType].id) {
-            console.log(itemType, itemData);
             const itemId = actor.data.data.externalData[itemType].id;
             const item = actor._getOwnedItem(itemId);
             const currentValue = updatedData.data.externalData[itemType].value;
@@ -27,9 +26,39 @@ const actorHooks = () => {
               switch (item.data.type) {
                 case "armor": {
                   if (itemType === "currentArmorBody") {
+                    const armorList = actor.getEquippedArmors("body");
+                    const updateList = [];
+                    const diff = item.data.data.bodyLocation.sp - item.data.data.bodyLocation.ablation - currentValue;
+                    armorList.forEach((a) => {
+                      if (item.data._id !== a.data._id) {
+                        const armorData = a.data;
+                        if (diff < 0) {
+                          armorData.data.bodyLocation.ablation = Math.max(armorData.data.bodyLocation.ablation + diff, 0);
+                        } else {
+                          armorData.data.bodyLocation.ablation = Math.min(armorData.data.bodyLocation.ablation + diff, armorData.data.bodyLocation.sp);
+                        }
+                        updateList.push(armorData);
+                      }
+                    });
+                    actor.updateEmbeddedEntity("OwnedItem", updateList);
                     item.data.data.bodyLocation.ablation = item.data.data.bodyLocation.sp - currentValue;
                   }
                   if (itemType === "currentArmorHead") {
+                    const armorList = actor.getEquippedArmors("head");
+                    const updateList = [];
+                    const diff = item.data.data.headLocation.sp - item.data.data.headLocation.ablation - currentValue;
+                    armorList.forEach((a) => {
+                      if (item.data._id !== a.data._id) {
+                        const armorData = a.data;
+                        if (diff < 0) {
+                          armorData.data.headLocation.ablation = Math.max(armorData.data.headLocation.ablation + diff, 0);
+                        } else {
+                          armorData.data.headLocation.ablation = Math.min(armorData.data.headLocation.ablation + diff, armorData.data.headLocation.sp);
+                        }
+                        updateList.push(armorData);
+                      }
+                    });
+                    actor.updateEmbeddedEntity("OwnedItem", updateList);
                     item.data.data.headLocation.ablation = item.data.data.headLocation.sp - currentValue;
                   }
                   if (itemType === "currentArmorShield") {
