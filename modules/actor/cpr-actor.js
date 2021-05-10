37882c56df3fde07e4ebfb24e19e80fdd440f7b6
twagner <tim1000.wagner@gmail.com>
2021-05-10 19:10:21 +0200

    Automatically link character to token upon creation, do not link mooks.

--- modules/actor/cpr-actor.js
+++ modules/actor/cpr-actor.js
@@ -54,6 +54,9 @@ export default class CPRActor extends Actor {
       LOGGER.trace("create | New Actor | CPRActor | called.");
       createData.items = [];
       createData.items = data.items.concat(await SystemUtils.GetCoreSkills(), await SystemUtils.GetCoreCyberware());
+      if (data.type === "character") {
+        createData.token = { actorLink: true };
+      }
     }
     super.create(createData, options);
   }
