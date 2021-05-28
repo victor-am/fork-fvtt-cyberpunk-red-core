fab35fd93db8bb5a4f32d714d024e9846f1ae66c
Ben King <benhking26@gmail.com>
2021-05-28 20:51:45 +1000

    Additional cleanup of code from local testing and missing spacing

--- modules/system/register-helpers.js
+++ modules/system/register-helpers.js
@@ -143,7 +143,7 @@ export default function registerHandlebarsHelpers() {
     LOGGER.trace(`Calling hasOptionalSlots`);
     if (optionSlots > 0) {
       LOGGER.trace(`hasOptionalSlots is greater than 0`);
-      return ("-" + installedOptionSlots + "/" + optionSlots + " " + SystemUtils.Localize("CPR.optionalslots"))
+      return ("- " + installedOptionSlots + "/" + optionSlots + " " + SystemUtils.Localize("CPR.optionalslots"))
     }
     else {
       LOGGER.trace(`hasOptionalSlots is 0`);
