9509042e719f55df3e8846d7cd059057e75766d6
twagner <tim1000.wagner@gmail.com>
2021-06-06 12:49:39 +0200

    Fixed broken container functions after the actor sheet lost some functions. Added some configuraion options for the GM.

--- modules/actor/sheet/cpr-container-sheet.js
+++ modules/actor/sheet/cpr-container-sheet.js
@@ -1,14 +1,20 @@
-/* global mergeObject $ game */
+/* global mergeObject $ game getProperty */
 import CPRActorSheet from "./cpr-actor-sheet.js";
 import LOGGER from "../../utils/cpr-logger.js";
 import SystemUtils from "../../utils/cpr-systemUtils.js";
+import CPRChat from "../../chat/cpr-chat.js";
 
 /**
  * Implement the sheet for containers and shop keepers.
  * @extends {CPRActorSheet}
  */
 export default class CPRContainerActorSheet extends CPRActorSheet {
-  /** @override */
+  /**
+   * Set the template, width and height of the window.
+   *
+   * @override
+   * @returns - sheet options merged with default options in ActorSheet
+   */
   static get defaultOptions() {
     LOGGER.trace("defaultOptions | CPRContainerActorSheet | Called.");
     return mergeObject(super.defaultOptions, {
@@ -18,12 +24,16 @@ export default class CPRContainerActorSheet extends CPRActorSheet {
     });
   }
 
-  /** @override */
+  /**
+   * Get actor data into a more convenient organized structure. This should be called sparingly in code.
+   * Only add new data points to getData when you need a complex struct, not when you only need to add
+   * new data points to shorten dataPaths.
+   *
+   * @override
+   * @returns {Object} data - a curated structure of actorSheet data
+   */
   getData() {
-    // TODO - Understand how to use getData and when.
-    // INFO - Only add new data points to getData when you need a complex struct.
-    // DO NOT add new data points into getData to shorten dataPaths
-    LOGGER.trace("ActorID getData | CPRContainerSheet | Called.");
+    LOGGER.trace("getData | CPRContainerSheet | Called.");
     const data = super.getData();
     data.isGM = game.user.isGM;
     data.userOwnedActors = game.actors.filter((a) => a.isOwner && a.type === "character");
@@ -37,27 +47,42 @@ export default class CPRContainerActorSheet extends CPRActorSheet {
     return data;
   }
 
-  /** @override */
+  /**
+   * Add listeners specific to the Container sheet. Remember additional listeners are added from the
+   * parent class, CPRActorSheet.
+   *
+   * @override
+   * @param {*} html - the DOM object
+   */
   activateListeners(html) {
     // Selection of trade partner
     html.find(".trade-with-dropdown").change((event) => this._setTradePartner(event));
+    // Create item in inventory
+    html.find(".item-create").click((event) => this._createInventoryItem(event));
+    // Toggle the state of a flag for the data of the checkbox
+    html.find(".checkbox-toggle").click((event) => this._checkboxToggle(event));
 
     super.activateListeners(html);
   }
 
-  /** @override */
+  /**
+   * Override the _itemAction() of the CPRActorSheet to add "purchase" action
+   * and remove non needed other action types.
+   *
+   *
+   * @async
+   * @private
+   * @callback
+   * @param {event} event - object capturing event data (what was clicked and where?)
+   */
   async _itemAction(event) {
     LOGGER.trace("_itemAction | CPRContainerSheet | Called.");
-    const item = this._getOwnedItem(this._getItemId(event));
+    const item = this._getOwnedItem(CPRActorSheet._getItemId(event));
     const actionType = $(event.currentTarget).attr("data-action-type");
     if (item) {
       switch (actionType) {
         case "delete": {
           await this._deleteOwnedItem(item);
-          break;
-        }
-        case "create": {
-          await this._createInventoryItem($(event.currentTarget).attr("data-item-type"));
           break;
         }
         case "purchase": {
@@ -76,11 +101,51 @@ export default class CPRContainerActorSheet extends CPRActorSheet {
     }
   }
 
+  /**
+   * Render the item card (chat message) when ctrl-click happens on an item link, or display
+   * the item sheet if ctrl was not pressed.
+   *
+   * @override
+   * @private
+   * @callback
+   * @param {Object} event - object capturing event data (what was clicked and where?)
+   */
+  _renderItemCard(event) {
+    LOGGER.trace("_renderItemCard | CPRContainerSheet | Called.");
+    const itemId = CPRActorSheet._getItemId(event);
+    const item = this.actor.items.find((i) => i.data._id === itemId);
+    if (event.ctrlKey) {
+      CPRChat.RenderItemCard(item);
+    } else {
+      const playersCanModify = getProperty(this.actor.data, "flags.cyberpunk-red-core.players-modify");
+      if (playersCanModify || game.user.isGM) {
+        item.sheet.render(true, { editable: true });
+      } else {
+        item.sheet.render(true, { editable: false });
+      }
+    }
+  }
+
+  /**
+   * This is the callback for setting the trade partner of the container.
+   * This given the knowledge, which actor to mofify upon purchase of items.
+   *
+   * @callback
+   * @private
+   * @param {} event - object capturing event data (what was clicked and where?)
+   */
   _setTradePartner(event) {
     LOGGER.trace("_setTradePartner | CPRContainerSheet | Called.");
     this.tradePartnerId = $(event.currentTarget).val();
   }
 
+  /**
+   * Handle the purchase of an item. The item is added to the actor specified as
+   * the tradePartner and money is deducted accordingly.
+   *
+   * @private
+   * @param {Item} item - object to be purchased
+   */
   async _purchaseItem(item) {
     LOGGER.trace("_purchaseItem | CPRContainerSheet | Called.");
     if (this.tradePartnerId === undefined || this.tradePartnerId === "") {
@@ -89,20 +154,62 @@ export default class CPRContainerActorSheet extends CPRActorSheet {
     }
 
     const tradePartnerActor = game.actors.get(this.tradePartnerId);
-    const price = item.data.data.price.market;
-    if (tradePartnerActor.data.data.wealth.value < price) {
-      SystemUtils.DisplayMessage("warn", SystemUtils.Localize("CPR.tradepricewarn"));
-      return;
-    }
-    const { amount } = item.data.data;
-    let reason = "";
-    if (amount > 1) {
-      reason = `${SystemUtils.Format("CPR.tradelogmultiple", { amount, name: item.name, price })} - ${game.user.name}`;
-    } else {
-      reason = `${SystemUtils.Format("CPR.tradelogsingle", { name: item.name, price })} - ${game.user.name}`;
+    if (!getProperty(this.actor.data, "flags.cyberpunk-red-core.items-free")) {
+      const price = item.data.data.price.market;
+      if (tradePartnerActor.data.data.wealth.value < price) {
+        SystemUtils.DisplayMessage("warn", SystemUtils.Localize("CPR.tradepricewarn"));
+        return;
+      }
+      const { amount } = item.data.data;
+      let reason = "";
+      if (amount > 1) {
+        reason = `${SystemUtils.Format("CPR.tradelogmultiple", { amount, name: item.name, price })} - ${game.user.name}`;
+      } else {
+        reason = `${SystemUtils.Format("CPR.tradelogsingle", { name: item.name, price })} - ${game.user.name}`;
+      }
+      await tradePartnerActor.deltaLedgerProperty("wealth", -1 * price, reason);
     }
     await tradePartnerActor.createEmbeddedDocuments("Item", [item.data]);
-    await tradePartnerActor.deltaLedgerProperty("wealth", -1 * price, reason);
-    await this._deleteOwnedItem(item, true);
+    if (!getProperty(this.actor.data, "flags.cyberpunk-red-core.infinite-stock")) {
+      await this._deleteOwnedItem(item, true);
+    }
+  }
+
+  /**
+   * Create an item in the inventory of the actor. The templates will hide this functionality
+   * if the GMs does not want to permit players to create their own items.
+   *
+   * @private
+   * @callback
+   * @param {Object} event - object capturing event data (what was clicked and where?)
+   */
+  async _createInventoryItem(event) {
+    LOGGER.trace("_createInventoryItem | CPRContainerSheet | Called.");
+    const itemType = $(event.currentTarget).attr("data-item-type");
+    const itemTypeNice = itemType.toLowerCase().capitalize();
+    const itemString = "ITEM.Type";
+    const itemTypeLocal = itemString.concat(itemTypeNice);
+    const itemName = `${SystemUtils.Localize("CPR.new")} ${SystemUtils.Localize(itemTypeLocal)}`;
+    const itemData = { name: itemName, type: itemType };
+    await this.actor.createEmbeddedDocuments("Item", [itemData]);
+  }
+
+  /**
+   * Set or unset a corresponding flag on the actor when a checkbox is clicked.
+   *
+   * @private
+   * @callback
+   * @param {Object} event - object capturing event data (what was clicked and where?)
+   */
+  _checkboxToggle(event) {
+    LOGGER.trace("_checkboxToggle | CPRContainerSheet | Called.");
+    const flagName = $(event.currentTarget).attr("data-flag-name");
+    const flag = getProperty(this.actor.data, `flags.cyberpunk-red-core.${flagName}`);
+    if (flag === undefined || flag === false) {
+      // if the flag was already set to firemode, that means we unchecked a box
+      this.actor.setFlag("cyberpunk-red-core", flagName, true);
+    } else {
+      this.actor.unsetFlag("cyberpunk-red-core", flagName);
+    }
   }
 }
