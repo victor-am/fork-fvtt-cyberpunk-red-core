1982ef6ded035b09df32278072628f93a2334c94
twagner <tim1000.wagner@gmail.com>
2021-07-03 15:18:09 +0200

    Added optinal test to see if the changelog changed, when merging to dev.

---
+++ tests/test-changelog-dev-changed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Download the original changelog from dev branch.
+wget http://gitlab.com/JasonAlanTerry/fvtt-cyberpunk-red-core/-/raw/master/CHANGELOG.md /tmp/CHANGELOG.md
+
+if cmp -s /tmp/CHANGELOG.md CHANGELOG.md; then       ## Checking if files are different.
+    echo "Changelog not changed"
+    exit 1                                           ## Job will fail
+else
+    echo "Changelog changed"
+    exit 0                                           ## Job will pass
+fi
