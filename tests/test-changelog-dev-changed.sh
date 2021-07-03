f52afc23ea593913888f58ee7b7f7347639930e4
twagner <tim1000.wagner@gmail.com>
2021-07-03 15:35:32 +0200

    Added debuging output to changelog test script.

--- tests/test-changelog-dev-changed.sh
+++ tests/test-changelog-dev-changed.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Download the original changelog from dev branch.
-wget http://gitlab.com/JasonAlanTerry/fvtt-cyberpunk-red-core/-/raw/dev/CHANGELOG.md /tmp/CHANGELOG.md
+mkdir tmp
+wget https://gitlab.com/JasonAlanTerry/fvtt-cyberpunk-red-core/-/raw/dev/CHANGELOG.md /tmp/CHANGELOG.md
+
+cat /tmp/CHANGELOG.md
 
 if cmp -s /tmp/CHANGELOG.md CHANGELOG.md; then       ## Checking if files are different.
     echo "Changelog not changed"
