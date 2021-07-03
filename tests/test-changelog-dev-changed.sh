2b3c04a1843bd890efe82978d3815713778b1199
twagner <tim1000.wagner@gmail.com>
2021-07-03 15:40:01 +0200

    Further changes to testing script

--- tests/test-changelog-dev-changed.sh
+++ tests/test-changelog-dev-changed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Download the original changelog from dev branch.
 mkdir tmp
-wget https://gitlab.com/JasonAlanTerry/fvtt-cyberpunk-red-core/-/raw/dev/CHANGELOG.md /tmp/CHANGELOG.md
+wget -O ./tmp/CHANGELOG.md https://gitlab.com/JasonAlanTerry/fvtt-cyberpunk-red-core/-/raw/dev/CHANGELOG.md
 
 cat /tmp/CHANGELOG.md
 
