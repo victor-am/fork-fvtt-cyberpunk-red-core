cbf4eaa05c47dadcb26decf4d9ced5e0293038d7
twagner <tim1000.wagner@gmail.com>
2021-07-04 16:49:44 +0200

    Added check to not stack items if they are upgraded.

--- modules/actor/cpr-actor.js
+++ modules/actor/cpr-actor.js
@@ -729,7 +729,7 @@ export default class CPRActor extends Actor {
   automaticallyStackItems(newItem) {
     const stackableItemTypes = ["ammo", "gear", "clothing"];
     if (stackableItemTypes.includes(newItem.type)) {
-      const match = this.items.find((i) => i.type === newItem.type && i.name === newItem.name);
+      const match = this.items.find((i) => i.type === newItem.type && i.name === newItem.name && i.data.data.upgrades.length === 0);
       if (match) {
         const newAmount = parseInt(match.data.data.amount, 10) + parseInt(newItem.data.data.amount, 10);
         const newPrice = match.data.data.price.market + newItem.data.data.price.market;
