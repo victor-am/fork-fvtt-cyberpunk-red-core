efa8faa9ee8641f8417ce79cbbf1e62266582d76
twagner <tim1000.wagner@gmail.com>
2021-07-06 16:50:35 +0200

    Fix minor typos in the new test scripts.

--- tests/test-misc.sh
+++ tests/test-misc.sh
@@ -7,17 +7,17 @@ echo "$result"
 count=$(echo "$result" | wc -l)
 if [ count != 0 ]
 then
-    echo "There are" $count "cases, where game.i18n was used instaead of our own localization."
+    echo "There are" $count "cases, where game.i18n was used instead of our own localization."
     succeed=0
 fi
 
-# Test if our notification code is used (exceptiuon for the migration)
+# Test if our notification code is used (exception for the migration)
 result=$(grep -r --exclude=cpr-systemUtils.js --exclude=migration.js "ui.notifications" ./modules/*)
 echo "$result"
 count=$(echo "$result" | wc -l)
 if [ count != 0 ]
 then
-    echo "There are" $count "cases, where ui.notifications was used instaead of our own SystemUtils.DisplayMessage."
+    echo "There are" $count "cases, where ui.notifications was used instead of our own SystemUtils.DisplayMessage."
     succeed=0
 fi
 
