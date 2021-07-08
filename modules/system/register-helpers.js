99a4181e8636df0a64562661acf9fefc4aa92d5a
twagner <tim1000.wagner@gmail.com>
2021-07-08 20:23:20 +0200

    Adjusted price listing in container to make it clear, what the single price and price for all items is.

--- modules/system/register-helpers.js
+++ modules/system/register-helpers.js
@@ -272,6 +272,9 @@ export default function registerHandlebarsHelpers() {
         const subtrahend = mathArgs.reduce((a, b) => a + b, 0);
         return minutend - subtrahend;
       }
+      case "product": {
+        return mathArgs.reduce((a, b) => a * b, 1);
+      }
       default:
         LOGGER.error(`!ERR: Not a Math function: ${mathFunction}`);
         return "null";
