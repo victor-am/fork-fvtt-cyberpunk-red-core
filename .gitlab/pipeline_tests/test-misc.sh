e52fcee57ce14e7628b63d8c9aedb59b5ab13093
twagner <tim1000.wagner@gmail.com>
2021-07-09 18:00:17 +0200

    Fixed test-misc script so it can actually succeed.

--- .gitlab/pipeline_tests/test-misc.sh
+++ .gitlab/pipeline_tests/test-misc.sh
@@ -5,7 +5,7 @@ succeed=1
 grep -rn --exclude=cpr-systemUtils.js --exclude=migration.js --exclude=pause-animation.js "game.i18n" ./modules/*
 # $(echo "$result" | wc -l) cannot count no appearances, thus grep has to be called again.
 count=$(grep -rn --exclude=cpr-systemUtils.js --exclude=migration.js --exclude=pause-animation.js "game.i18n" ./modules/* | wc -l)
-if [ count != 0 ]
+if [ $count != 0 ]
 then
     echo "There are" $count "cases, where game.i18n was used instead of our own localization."
     succeed=0
@@ -14,14 +14,14 @@ fi
 # Test if our notification code is used (exception for the migration)
 grep -rn --exclude=cpr-systemUtils.js --exclude=migration.js "ui.notifications" ./modules/*
 count=$(grep -rn --exclude=cpr-systemUtils.js --exclude=migration.js "ui.notifications" ./modules/* | wc -l)
-if [ count != 0 ]
+if [ $count != 0 ]
 then
     echo "There are" $count "cases, where ui.notifications was used instead of our own SystemUtils.DisplayMessage."
     succeed=0
 fi
 
 # Check if any test above failed and fail or succed the job accordingly.
-if [ succeed != 1 ]
+if [ $succeed != 1 ]
 then
     exit 1
 fi
