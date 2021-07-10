8427fca64555d14a41261e16c9b06c86c55a100d
twagner <tim1000.wagner@gmail.com>
2021-07-10 10:36:16 +0200

    Extended the linter rule to also check for the function name in the trace argument, moved it into the .gitlab/pipeline_tests/ folder.

--- .gitlab/pipeline_tests/eslint-plugin-foundry-cpr/index.js
+++ .gitlab/pipeline_tests/eslint-plugin-foundry-cpr/index.js
@@ -1,7 +1,9 @@
 module.exports.rules = {
   "logger-after-function-definition": (context) => ({
     MethodDefinition: (node) => {
-      // context.report(node, "After a method definition a LOGGER.trace call is needed.");
+      const message1 = "After a method definition a LOGGER.trace call is needed.";
+      const message2 = "The LOGGER.trace call after a method definition needs to contain the function name.";
+      const functionName = node.key.name;
       if (
         node
         && node.value
@@ -11,22 +13,32 @@ module.exports.rules = {
         && node.value.body.body
       ) {
         if (node.value.body.body[0].type !== "ExpressionStatement") {
-          context.report(node, "After a method definition a LOGGER.trace call is needed.");
+          context.report(node, message1);
         } else if (
           node.value.body.body[0].expression
           && node.value.body.body[0].expression.type !== "CallExpression"
         ) {
-          context.report(node, "After a method definition a LOGGER.trace call is needed.");
+          context.report(node, message1);
         } else if (
           node.value.body.body[0].expression.callee
           && node.value.body.body[0].expression.callee.type !== "MemberExpression"
         ) {
-          context.report(node, "After a method definition a LOGGER.trace call is needed.");
+          context.report(node, message1);
         } else if (
           node.value.body.body[0].expression.callee.object
           && node.value.body.body[0].expression.callee.object.name !== "LOGGER"
         ) {
-          context.report(node, "After a method definition a LOGGER.trace call is needed.");
+          context.report(node, message1);
+        } else if (
+          node.value.body.body[0].expression.arguments[0]
+          && node.value.body.body[0].expression.arguments[0].type !== "Literal"
+        ) {
+          context.report(node.value.body.body[0].expression, message2);
+        } else if (
+          node.value.body.body[0].expression.arguments[0].value
+          && !node.value.body.body[0].expression.arguments[0].value.includes(functionName)
+        ) {
+          context.report(node.value.body.body[0].expression, message2);
         }
       }
     },
