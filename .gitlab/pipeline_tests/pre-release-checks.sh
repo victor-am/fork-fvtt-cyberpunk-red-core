c19eb45ce038024538ff6f72624adc017bbdc713
Ben King <benhking26+gitlab@gmail.com>
2022-03-28 07:21:11 +1100

    Add improvements to pre-release pipeline

--- .gitlab/pipeline_tests/pre-release-checks.sh
+++ .gitlab/pipeline_tests/pre-release-checks.sh
@@ -9,16 +9,16 @@ changelog=CHANGELOG.md
 grep "WIP" $changelog > /dev/null
 if [ $? -eq 0 ]
 then
-	errors+=("WIP exists in the changelog")
+	errors+=("The string 'WIP' exists in the changelog...")
 fi
 
 # Verify the system version is the same as the download zip
 systemfile=system.json
 system_version=$(grep version $systemfile | awk '{print $2}' | sed 's/^.//;s/..$//')
 download_zip=$(cat $systemfile | jq .download)
-if [[ $download_zip != *$system_version* ]]
+if [[ $download_zip != "\"https://gitlab.com/JasonAlanTerry/fvtt-cyberpunk-red-core/-/archive/v${system_version}/fvtt-cyberpunk-red-core-v${system_version}.zip\"" ]]
 then
-  errors+=("The expected system version ($system_version) isn't in the download.zip value")
+  errors+=("The expected system version ($system_version) isn't in the download.zip value in 'system.json'...")
 fi
 
 if [ ${#errors[@]} -gt 0 ]
@@ -30,4 +30,4 @@ then
   exit 1
 fi
 
-echo "No problems found. You should now merge dev into master!"
+echo "No problems found. You can now merge dev into master!"
