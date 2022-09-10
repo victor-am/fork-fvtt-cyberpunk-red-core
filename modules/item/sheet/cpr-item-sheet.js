a858f9abb253a1f88defc804b4402d702824b521
dsorrent <Darin.Sorrentino@gmail.com>
2022-09-10 11:18:10 -0400

    For some reason we were storing what programs that were installed on the Cyberdeck on the Actor.  I suspect this was just a convenience thing to be accessed from the HBS files, but it is not really needed and complicates the token resource drop down.

--- modules/item/sheet/cpr-item-sheet.js
+++ modules/item/sheet/cpr-item-sheet.js
@@ -712,7 +712,7 @@ export default class CPRItemSheet extends ItemSheet {
     programList = actor.itemTypes.program;
 
     // Remove all programs that are installed somewhere other than this deck
-    actor.system.programsInstalled.forEach((programId) => {
+    this.item.system.programs.installed.forEach((programId) => {
       const onDeck = installedPrograms.filter((p) => p._id === programId);
       if (onDeck.length === 0) {
         programList = programList.filter((p) => p.id !== programId);
