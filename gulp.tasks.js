0bccc78d22e6842a8a78a117abb802a7420545a8
cyberwomble <cyberwomble@markperry.co.uk>
2022-09-24 19:37:56 +0100

    chore(build): fix gulp scripts

--- gulp.tasks.js
+++ gulp.tasks.js
@@ -1,62 +1,38 @@
 const fs = require("fs-extra");
 const path = require("path");
 const del = require("del");
+const log = require("fancy-log");
 
 // Gulp
 const gulp = require("gulp");
 const less = require("gulp-less");
 
+const DEFAULT_DESTINATION_FOLDER = "dist";
+
 // Config
 // Read foundryconfig.json if it exists, else use default vaules
 // This allows for devs to overwrite `dist/` as the build dir to
 // wherever they need it for their local Foundry install.
-const CONFIG_FILE = path.resolve("foundryconfig.json");
-const config = fs.existsSync(CONFIG_FILE) ?
-                 fs.readJSONSync(CONFIG_FILE) :
-                 {"dataPath": path.resolve(process.cwd(), "dist")}
-const destFolder = config.dataPath;
+function resolveDestinationFolder() {
+  const localConfigPath = path.resolve("foundryconfig.json");
+  const localConfigExists = fs.existsSync(localConfigPath);
 
-console.log(destFolder);
+  if (localConfigExists) {
+    return fs.readJSONSync(localConfigPath).dataPath;
+  }
+  return DEFAULT_DESTINATION_FOLDER;
+}
 
-const sourceFiles = [
+const destFolder = resolveDestinationFolder();
+
+const assetsToCopy = [
   {
-    from: "src/cpr.js",
+    from: "src/**/*",
     to: "",
-  },
-  {
-    from: "src/environment.js",
-    to: "",
-  },
-  {
-    from: "src/template.json",
-    to: "",
-  },
-];
-const sourceFolders = [
-  {
-    from: "src/assets/**/*",
-    to: "assets",
-  },
-  {
-    from: "src/fonts/**/*",
-    to: "fonts",
-  },
-  {
-    from: "src/lang/**/*",
-    to: "lang",
-  },
-  {
-    from: "src/templates/**/*",
-    to: "templates",
-  },
-  {
-    from: "src/modules/**/*",
-    to: "packs",
   },
 ];
 
 async function cleanDist() {
-
   const files = fs.readdirSync(destFolder);
   const results = [];
   for (const file of files) {
@@ -69,37 +45,38 @@ async function cleanDist() {
 function compileLess() {
   return gulp.src("src/less/main.less")
     .pipe(less({ javascriptEnabled: true }))
-    .pipe(gulp.dest(path.resolve(destFolder)));
+    .pipe(gulp.dest(path.resolve(destFolder)))
+    .on("end", () => { log("CPR - CompileLess"); });
 }
 
 async function copyAssets() {
-  [...sourceFiles, ...sourceFolders].forEach((asset) => {
+  assetsToCopy.forEach((asset) => {
     gulp.src(asset.from).pipe(gulp.dest(path.resolve(destFolder, asset.to)));
   });
 }
 
 async function updateSystem() {
   const version = process.env.CI_COMMIT_TAG ? process.env.CI_COMMIT_TAG : "0.0.0";
-  const project_path = process.env.CI_PROJECT_PATH ? process.env.CI_PROJECT_PATH : "dev";
+  const projectPath = process.env.CI_PROJECT_PATH ? process.env.CI_PROJECT_PATH : "dev";
   const systemRaw = fs.readFileSync("src/system.json");
-  let system = JSON.parse(systemRaw);
+  const system = JSON.parse(systemRaw);
 
   system.version = version;
-  system.url = `https://gitlab.com/${project_path}`;
-  system.manifest = `https://gitlab.com/${project_path}/-/jobs/artifacts/${version}/raw/system.json?job=build`;
-  system.download = `https://gitlab.com/${project_path}/-/jobs/artifacts/${version}/raw/cpr.zip?job=build`;
+  system.url = `https://gitlab.com/${projectPath}`;
+  system.manifest = `https://gitlab.com/${projectPath}/-/jobs/artifacts/${version}/raw/system.json?job=build`;
+  system.download = `https://gitlab.com/${projectPath}/-/jobs/artifacts/${version}/raw/cpr.zip?job=build`;
+
   fs.writeFileSync(path.resolve(destFolder, "system.json"), JSON.stringify(system, null, 2));
 }
 
 async function watch() {
   // Helper - watch the pattern, copy the output on change
   function watcher(pattern, out) {
-    gulp.watch(pattern).on("change", () => gulp.src(pattern).pipe(gulp.dest(path.resolve(destFolder, out))));
+    gulp.watch(pattern).on("change", () => gulp.src(pattern).pipe(gulp.dest(path.resolve(destFolder, out))).on("end", () => log("CPR - Watch")));
   }
 
-  sourceFolders.forEach((folder) => watcher(folder.from, folder.to));
-  sourceFiles.forEach((file) => watcher(file.from, file.to));
-  gulp.watch("src/**/*.less").on("change", async () => compileLess());
+  assetsToCopy.forEach((folder) => watcher(folder.from, folder.to));
+  gulp.watch("src/**/*.less").on("change", () => compileLess());
 }
 
 exports.clean = cleanDist;
